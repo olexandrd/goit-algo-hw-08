4e158488b09a3d9fa9ef87dde78983588c6655d7
Olexandrd <olexandrd@gmail.com>
2024-01-25 22:11:26 +0200

    fixes according to review

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ import heapq
 
 def calculate_length(fragment_list: list) -> int:
     heapq.heapify(fragment_list)
+    costs = list()
+
     while len(fragment_list) > 1:
-        heapq.heappush(
-            fragment_list, heapq.heappop(fragment_list) + heapq.heappop(fragment_list)
-        )
-    return heapq.heappop(fragment_list)
+        shortest_1 = heapq.heappop(fragment_list)
+        shortest_2 = heapq.heappop(fragment_list)
+        cost = shortest_1 + shortest_2
+        costs.append(cost)
+        heapq.heappush(fragment_list, cost)
+    return costs
 
 
 def main():
     lst = [4, 6, 12, 3, 5]
-    print(calculate_length(lst))
+    connection_costs = calculate_length(lst)
+    print(
+        f"Connection order:\n" + " => ".join([str(order) for order in connection_costs])
+    )
+    print("Total costs:", sum(connection_costs))
 
 
 if __name__ == "__main__":
